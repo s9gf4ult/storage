37a7b81a9adab52edd71c11d504f6db52a44f623
segfault <s9gf4ult@gmail.com>
2014-02-17 22:40:57 +0600

    generate storage_items_controller

---
+++ app/controllers/storage_items_controller.rb
@@ -0,0 +1,25 @@
+class StorageItemsController < ApplicationController
+  unloadable
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
