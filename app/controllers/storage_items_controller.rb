34fa1b20d579348ea0e605b115d928f8109e2e93
segfault <s9gf4ult@gmail.com>
2014-02-20 11:59:33 +0600

    Changes to be committed: 	modified:   app/controllers/storage_items_controller.rb 	new file:   app/views/storage_items/_form.html.erb 	modified:   app/views/storage_items/index.html.erb 	modified:   app/views/storage_items/new.html.erb 	modified:   config/routes.rb 	modified:   init.rb

--- app/controllers/storage_items_controller.rb
+++ app/controllers/storage_items_controller.rb
@@ -1,25 +1,57 @@
 class StorageItemsController < ApplicationController
   unloadable
 
+  before_filter :find_project, :only => [:index, :new]
+  before_filter :find_item, :only => [:show, :edit, :update, :delete]
 
   def index
+    @items = StorageItem.joins(:issue => [:project]).where("projects.id" => @project.id)
   end
 
   def show
+    @project = @item.issue.try(:project)
   end
 
   def edit
+    @project = @item.issue.try(:project)
   end
 
   def new
+    @item = StorageItem.new
   end
 
   def create
+    @item = StorageItem.build params[:storage_item]
+    respond_to do |format|
+      if @item.save
+        head 200                  #  FIXME:
+      end
+    end
   end
 
   def update
+    if @item.update(params[:storage_item])
+      head 200                  #  FIXME:
+    end
   end
 
-  def delete
+  def destroy
+    if @item.destroy
+      head 200                  #  FIXME:
+    end
+  end
+
+  protected
+
+  def find_project
+    @project = Project.find params[:project_id]
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
+  def find_item
+    @item = StorageItem.find params[:id]
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
--- init.rb
+++ init.rb
@@ -5,4 +5,10 @@ Redmine::Plugin.register :storage do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  permission :storage_items, {:storage_items => [:index]}, :public => true
+  menu(:project_menu, :storage_items,
+       {:controller => "storage_items", :action => "index"},
+       :caption => "Items",
+       :param => :project_id)
 end
